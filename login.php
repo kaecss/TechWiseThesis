9672d6585bcc2ebf04cd1c89e34dbe33bc0fb2f4
VinceGUH <118741036+VinceGUH@users.noreply.github.com>
2024-06-22 20:42:23 +0800

    commit

--- login.php
+++ login.php
@@ -148,7 +148,7 @@ if (isset($_POST['signup'])) {
     <meta charset="UTF-8">
     <title>USER Form</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="login-user.css">
+    <link rel="stylesheet" href="user-login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <style>
